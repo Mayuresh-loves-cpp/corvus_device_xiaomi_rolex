ec9c216f760bc263c6896d05b5206a4c0b026844
Mayuresh-loves-cpp <mvshinde640@gmail.com>
2021-01-01 19:58:07 +0530

    rolex: corvus dt bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,8 +14,8 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_rolex.mk
+    $(LOCAL_DIR)/corvus_rolex.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_rolex-user \
-    aosp_rolex-userdebug
+    corvus_rolex-user \
+    corvus_rolex-userdebug
